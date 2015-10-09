cfc2e1c8202f563120346b6fc8c7c9784d0ad289
Po Chen <chenpaul914@gmail.com>
2015-10-09 16:15:00 +1100

    github setup

--- app/services/scouts/github.rb
+++ app/services/scouts/github.rb
@@ -1,5 +1,11 @@
 class Scouts::Github < Scout
-  base_uri 'api.github.com'
+  base_uri 'https://api.github.com'
+
+  headers 'User-Agent' => ENV['github_app_name']
+  default_params(
+    client_id: ENV['github_client_id'],
+    client_secret: ENV['github_client_secret']
+  )
 
   def perform
     api.get("/repos/#{@lib.name}/git/refs/tags")
