c3ac38d92b71cf15a86216bbf538722c57c92508
Po Chen <chenpaul914@gmail.com>
2015-10-11 21:00:25 +1100

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008123556) do
+ActiveRecord::Schema.define(version: 20151011095837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,5 +29,15 @@ ActiveRecord::Schema.define(version: 20151008123556) do
   add_index "libs", ["checked_at"], name: "index_libs_on_checked_at", using: :btree
   add_index "libs", ["name"], name: "index_libs_on_name", using: :btree
   add_index "libs", ["type"], name: "index_libs_on_type", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
 
 end
