ba42f9da8323ab19fc68b32e9889f34d2afa4830
Po Chen <chenpaul914@gmail.com>
2015-11-25 23:09:01 +1100

    preloads

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,24 @@
 class HomeController < ApplicationController
   def index
+    @libs = [
+      {
+        name: 'Ruby Gem',
+        devicon: 'ruby'
+      },
+      {
+        name: 'Python Package',
+        devicon: 'python'
+      },
+      {
+        name: 'JS Package',
+        devicon: 'javascript'
+      },
+      {
+        name: 'GitHub Repo',
+        devicon: 'github'
+      }
+    ].each do |lib|
+      lib[:key] = lib[:name].downcase.gsub(/\s+/, '-')
+    end
   end
 end
