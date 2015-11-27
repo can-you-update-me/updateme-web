6bd41594a763dfec8682a0b706f27d028adb1131
Po Chen <chenpaul914@gmail.com>
2015-11-27 17:34:16 +1100

    add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Libs::RubyGem.create([
+  { name: 'rails' },
+  { name: 'rack' },
+  { name: 'rake' },
+  { name: 'zeus' },
+  { name: 'faraday' },
+  { name: 'byebug' },
+  { name: 'puma' },
+  { name: 'figaro' }
+])
+
+Libs::JsPackage.create([
+  { name: 'angular' },
+  { name: 'webpack' },
+  { name: 'lodash' },
+  { name: 'postcss' },
+  { name: 'react' },
+  { name: 'angular2' },
+  { name: 'express' },
+  { name: 'karma' }
+])
+
+Libs::PythonPackage.create([
+  { name: 'requests' },
+  { name: 'django' },
+  { name: 'flask' },
+  { name: 'docopt' },
+  { name: 'ipython' },
+  { name: 'jinja2' },
+  { name: 'sqlalchemy' },
+  { name: 'scipy' }
+])
+
+Libs::GithubRepo.create([
+  { name: 'goupdateme/updateme' },
+  { name: 'docker/docker' },
+  { name: 'docker/compose' },
+  { name: 'docker/machine' },
+  { name: 'docker/swarm' },
+  { name: 'ClusterHQ/flocker' },
+  { name: 'antirez/redis' },
+  { name: 'princemaple/scroll-trigger' }
+])
