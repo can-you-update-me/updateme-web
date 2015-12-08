27b56e6bfbb169323452d4a33df0086726546fdd
Po Chen <chenpaul914@gmail.com>
2015-12-08 13:58:04 +1100

    handle action outside QuickToast

--- app/frontend/javascripts/utils/quick_toast.js
+++ app/frontend/javascripts/utils/quick_toast.js
@@ -1,19 +1,18 @@
 angular.module('updateme')
 .factory('QuickToast', function($mdToast) {
   return function(text, {
-    callback = null,
     action = 'OK',
     hideDelay = 5000,
     highlightAction = false,
     position = 'top right'
   } = {}) {
-    $mdToast.show(
+    return $mdToast.show(
       $mdToast.simple()
         .textContent(text)
         .action(action)
         .highlightAction(highlightAction)
         .position(position)
         .hideDelay(hideDelay)
-    ).then(callback);
+    );
   };
 });
