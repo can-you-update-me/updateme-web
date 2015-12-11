92db9685503704c336609bf557ab31d5139f27a5
Po Chen <chenpaul914@gmail.com>
2015-12-11 17:15:29 +1100

    camelize preload keys

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
   def json_preload(load_key, hash)
-    content_tag :script, JSON.generate(hash).html_safe,
-      type: 'text/json-preload', charset: 'UTF-8', json_preload: load_key
+    content_tag(
+      :script,
+      JSON.generate(hash).html_safe,
+      charset: 'UTF-8',
+      type: 'text/json-preload',
+      json_preload: load_key.camelize(:lower)
+    )
   end
 end
