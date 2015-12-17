c8c087b5090c6ad79a77c73753944e475423ae56
Po Chen <chenpaul914@gmail.com>
2015-12-17 20:59:57 +1100

    return subscriptions on login

--- app/services/who_am_i.rb
+++ app/services/who_am_i.rb
@@ -10,6 +10,7 @@ class WhoAmI < BaseService
       name: current_user.name,
       email: current_user.email,
       email_digest: Digest::MD5.hexdigest(current_user.email),
+      subscriptions: Subscription.where(user: current_user),
       token: JWT.encode(
         {
           user_id: current_user.id,
